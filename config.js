3f056f453f22350e8e4218d70897e373e873875d
lstralaleritas-ship-it <lstralaleritas@gmail.com>
2026-02-24 12:32:37 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const TOKEN = "MTQ3NTkyMTU0OTkwNjQxNTY1Ng.GogszY.VOpIazg_oTVzr9m8SjVi_TrpPQ8KJ7B6NJwAyc";
+export const TOKEN = process.env.TOKEN;
 export const MAX_FILE_SIZE = 2 * 1024 * 1024; // 2 MB
